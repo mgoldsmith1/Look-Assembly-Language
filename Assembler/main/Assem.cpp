6cfeca2b7e5fc1b75540b2a2951953374aa8e675
Matthew Goldsmith <31556412+mgoldsmith1@users.noreply.github.com>
2018-02-16 00:01:24 -0600

    Update Assem.cpp

--- Assembler/main/Assem.cpp
+++ Assembler/main/Assem.cpp
@@ -20,11 +20,12 @@ int main( int argc, char *argv[] )
     assem.PassII( );
     
     // Run the emulator on the Look assembly program that came from the translation.
-	assem.RunEmulator();
+    assem.RunEmulator();
    
     // Terminate indicating all is well.  If there is an unrecoverable error, the 
     // program will terminate at the point that it occurred.
     return 0;
-}// Assemb.cpp : Defines the entry point for the console application.
-//
+}
+// Assem.cpp : Defines the entry point for the console application.
+
 
