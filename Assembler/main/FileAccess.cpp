e42a4da4a85670035ca455855c22eb68e5110ce2
Matthew Goldsmith <31556412+mgoldsmith1@users.noreply.github.com>
2018-02-16 00:11:32 -0600

    Update FileAccess.cpp

--- Assembler/main/FileAccess.cpp
+++ Assembler/main/FileAccess.cpp
@@ -7,31 +7,30 @@
 // Don't forget to comment the function headers.
 FileAccess::FileAccess( int argc, char *argv[] )
 {
-	
     // Check that there is exactly one run time parameter.
     if( argc != 2 ) {
         cerr << "Usage: Assem <FileName>" << endl;
         exit( 1 );
     }
-		// Open the file.  One might question if this is the best place to open the file.
-		// One might also question whether we need a file access class.
-		//
-	m_sfile.open(argv[1], ios::in);
+    // Open the file.  One might question if this is the best place to open the file.
+    // One might also question whether we need a file access class.
+    //
+    m_sfile.open(argv[1], ios::in);
 
     // If the open failed, report the error and terminate.
     if( ! m_sfile ) {
         cerr << "Source file could not be opened, assembler terminated."
-            << endl;
+             << endl;
         exit( 1 ); 
     }
-	else{
-		cout << "Source file found" << endl;
-		//m_sfile.read();
-		while ( !m_sfile.eof() ) {		
+    else{
+    	cout << "Source file found" << endl;
+
+	while ( !m_sfile.eof() ) {		
            m_sfile >> m_buff;
-		   GetNextLine(m_buff);
-		}
+           GetNextLine(m_buff);
 	}
+    }
 }
 FileAccess::~FileAccess( )
 {
@@ -50,7 +49,7 @@ bool FileAccess::GetNextLine( string &a_buff )
 void FileAccess::rewind( ) //Going back to the beginning 
 {
     // Clean the end of file flag and go back to the beginning of the file.
-    m_sfile.clear();	//***Clears the bits in the ifstream?
-    m_sfile.seekg( 0, ios::beg ); //***Means next place we ae reading is from the beginning of the file 
+    m_sfile.clear();	          //*** Clears the bits in the ifstream?
+    m_sfile.seekg( 0, ios::beg ); //*** Means next place we ae reading is from the beginning of the file 
 }
     
