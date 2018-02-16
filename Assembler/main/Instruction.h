5fc8e6e4090a2483128ac89a3fc77a7d31b4bc80
Matthew Goldsmith <31556412+mgoldsmith1@users.noreply.github.com>
2018-02-16 00:29:05 -0600

    Update Instruction.h

--- Assembler/main/Instruction.h
+++ Assembler/main/Instruction.h
@@ -16,31 +16,32 @@ using namespace boost;
 class Instruction {
 
 public:
- 	   Instruction( ) { };
-	  ~Instruction( ) { };
+        Instruction( ) { };
+       ~Instruction( ) { };
 
-    // Codes to indicate the type of instruction we are processing. 
-    enum InstructionType {
-        ST_MachineLanguage, // A machine language instruction.
-        ST_AssemblerInstr,  // Assembler Language instruction.
-        ST_Comment,         // Comment or blank line
-        ST_End              // end instruction.
-    };
-    // Record and parse the Instruction.
-    InstructionType RecordInstruction( string &a_buff );
+        // Codes to indicate the type of instruction we are processing. 
+        enum InstructionType {
+        	ST_MachineLanguage, // A machine language instruction.
+        	ST_AssemblerInstr,  // Assembler Language instruction.
+        	ST_Comment,         // Comment or blank line
+        	ST_End              // end instruction.
+    	};
+    	// Record and parse the Instruction.
+    	InstructionType RecordInstruction( string &a_buff );
 
-    // Compute the location of the next instruction.
-    int LocationNextInstruction( int a_loc );
+    	// Compute the location of the next instruction.
+    	int LocationNextInstruction( int a_loc );
 
-    // To access the label
-    inline string &GetLabel( ) {
+    	// To access the label
+    	inline string &GetLabel( ) {
 	    
-        return m_Label;
-    };
-    // To determine if a label is blank.
-    inline bool isLabel( ) {
-        return ! m_Label.empty();
-    };
+    		return m_Label;
+    	};
+	
+    	// To determine if a label is blank.
+    	inline bool isLabel( ) {
+        	return ! m_Label.empty();
+    	};
 
 	void setOpCodeValue(int opcodeValue){
 		m_OpCode = opcodeValue;
@@ -72,22 +73,21 @@ public:
 	friend inline bool isInteger(const std::string & s);
 
 private:
-		string m_Label;					// The label.
-		string m_OpCodeStringValue;     // The symbolic op code.
+	string m_Label;			// The label.
+	string m_OpCodeStringValue;     // The symbolic op code.
 
-		string m_OperandStringValue;
-		string m_Operand;				// The operand.
-		string m_instruction;			// The original instruction.
-		string m_comment;
+	string m_OperandStringValue;
+	string m_Operand;		// The operand.
+	string m_instruction;		// The original instruction.
+	string m_comment;
 
-		int m_OpCode;				    // The numerical value of the OpCode.
-		InstructionType m_type;			// The type of instruction.
+	int m_OpCode;		        // The numerical value of the OpCode.
+	InstructionType m_type;		// The type of instruction.
 
-		int m_OperandValue;				// The value of the operand if it is numeric. 
-		int m_detLoc;
+	int m_OperandValue;		// The value of the operand if it is numeric. 
+	int m_detLoc;
 		
-		string m_LeftVariableValue;
-		string m_RightVariableValue;
-
+	string m_LeftVariableValue;
+	string m_RightVariableValue;
 };
 #endif
