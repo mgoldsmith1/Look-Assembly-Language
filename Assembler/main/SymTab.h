9c0eaed9c22f02d5249e8e9409b8d31962e79551
Matthew Goldsmith <31556412+mgoldsmith1@users.noreply.github.com>
2018-02-16 00:32:46 -0600

    Update SymTab.h

--- Assembler/main/SymTab.h
+++ Assembler/main/SymTab.h
@@ -7,23 +7,23 @@
 class SymbolTable {
 
 public:
-    SymbolTable( ) {};
-    ~SymbolTable( ) {};
+        SymbolTable( ) {};
+       ~SymbolTable( ) {};
 
-    // Add a new symbol to the symbol table.
-    void AddSymbol( string &a_symbol, int a_loc );
+        // Add a new symbol to the symbol table.
+        void AddSymbol( string &a_symbol, int a_loc );
 
-    // Display the symbol table.
+        // Display the symbol table.
 	void DisplaySymbolTable( );
 
-    // Lookup a symbol in the symbol table.				
+        // Lookup a symbol in the symbol table.				
 	string LookupSymbol( int &a_loc);
 	int DisplaySymbolLocationTable();
 	string DisplaySymbol(int &a_loc);
     
 	// This is the actual symbol table.  The symbol is the key to the map.
 	map<string, int> m_symbolTable;
+	
 private:
 	
-
 };
