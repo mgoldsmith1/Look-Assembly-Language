2eb682115a8fb51810b00a4a9ea29e90549ff05d
Matthew Goldsmith <31556412+mgoldsmith1@users.noreply.github.com>
2018-02-16 00:24:59 -0600

    Update Instruction.cpp

--- Assembler/main/Instruction.cpp
+++ Assembler/main/Instruction.cpp
@@ -21,110 +21,108 @@ void Instruction::instruction_identifier(const char * str)
 		///////////////////////////// Operands //////////////////////////////////////
 		/////////////////////////////////////////////////////////////////////////////
 		case flag("det"): 
-							m_type = InstructionType::ST_AssemblerInstr;
-							m_Operand = "det";
-							break;
+			              m_type = InstructionType::ST_AssemblerInstr;
+		    		      m_Operand = "det";
+				      break;
 
 		case flag("declare"): 
-							m_type = InstructionType::ST_AssemblerInstr;
-							m_Operand = "declare";
-							break;
+				      m_type = InstructionType::ST_AssemblerInstr;
+				      m_Operand = "declare";
+				      break;
 		case flag("ds"):
-							m_type = InstructionType::ST_AssemblerInstr;
-							m_Operand = "ds"; 
-							break;
+				      m_type = InstructionType::ST_AssemblerInstr;
+				      m_Operand = "ds"; 
+				      break;
 		case flag("end"):
-							m_type = InstructionType::ST_End;
-							m_Operand = "end";
-							break;
+				      m_type = InstructionType::ST_End;
+			   	      m_Operand = "end";
+				      break;
 
 		/////////////////////////////////////////////////////////////////////////////
 		////////////////////////////// OpCodes //////////////////////////////////////
 		/////////////////////////////////////////////////////////////////////////////
 
 		case flag("add"): 
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "add";
-							m_OpCode = 01;
-							break;
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "add";
+				      m_OpCode = 01;
+				      break;
 
 		case flag("sub"): 
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "sub";
-						    m_OpCode = 02;
-							break;
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "sub";
+				      m_OpCode = 02;
+				      break;
 		case flag("mult"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "mult";
-							m_OpCode = 03;
-							break;
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "mult";
+				      m_OpCode = 03;
+				      break;
 		case flag("div"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "div";
-							m_OpCode = 04;
-							break;
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "div";
+				      m_OpCode = 04;
+				      break;
 		case flag("pack"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "pack";
-							m_OpCode = 05;
-							break;
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "pack";
+				      m_OpCode = 05;
+				      break;
 
-		case flag("acc"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "acc";
-							m_OpCode = 06;
-							break;
+		case flag("acc"): 
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "acc";
+	   			      m_OpCode = 06;
+				      break;
 
 		case flag("look"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "look";
-							m_OpCode = 07;
-							break;
+	    		              m_type = InstructionType::ST_MachineLanguage;
+	   			      m_OpCodeStringValue = "look";
+				      m_OpCode = 07;
+		   	 	      break;
 
 		case flag("write"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "write";
-							m_OpCode = 8;
-							break;
-
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "write";
+				      m_OpCode = 8;
+				      break;
 		case flag("b"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "b";
-							m_OpCode = 9;
-							break;
+				      m_type = InstructionType::ST_MachineLanguage;
+			              m_OpCodeStringValue = "b";
+				      m_OpCode = 9;
+				      break;
 		case flag("bm"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "bm";
-							m_OpCode = 10;
-							break;
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "bm";
+				      m_OpCode = 10;
+				      break;		
 		case flag("bz"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "bz";
-							m_OpCode = 11;
-							break;
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "bz";
+				      m_OpCode = 11;
+				      break;
 		case flag("bp"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "bp";
-							m_OpCode = 12;
-							break;
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "bp";
+				      m_OpCode = 12;
+				      break;
 		case flag("halt"):
-							m_type = InstructionType::ST_MachineLanguage;
-							m_OpCodeStringValue = "halt";
-							m_OpCode = 13;
-							break;
+				      m_type = InstructionType::ST_MachineLanguage;
+				      m_OpCodeStringValue = "halt";
+				      m_OpCode = 13;
+				      break;
 		
 		default:			
-				m_Label = str;
-				if (!m_Label.empty() && !m_Label.find_first_of(";")) {
-					GetLabel() = str;
-					m_type = InstructionType::ST_Comment;
-					break;
-				}
-				else {
-		
-				}
-
+			m_Label = str;
+			if (!m_Label.empty() && !m_Label.find_first_of(";")) {
+				GetLabel() = str;
+				m_type = InstructionType::ST_Comment;
 				break;
+			}
+			else {
+				
+			}
+			break;
 	};
 
 }
@@ -139,15 +137,13 @@ inline bool isInteger(const std::string & s)
 }
 Instruction::InstructionType Instruction::GetCommentInstruction(string &a_comment) {
 
-	//getline(cin, a_comment); //text
-
 	if (a_comment.find_first_of(";") == string::npos)
 	{
 		m_comment = a_comment;//.substr(0, 1);
 		m_type = InstructionType::ST_Comment;
 		
 	}
-    return m_type;
+        return m_type;
 }
 
 // Record and parse the Instruction.
@@ -156,7 +152,7 @@ Instruction::InstructionType Instruction::RecordInstruction(string &a_buff)
 	string temp, first, last;
 	stringstream instructionStream(a_buff);
 	const int MAX_INSTRUCTIONS = 100;
-	string instructionLineArray[MAX_INSTRUCTIONS]; //100
+	string instructionLineArray[MAX_INSTRUCTIONS]; 
  	int i = 0;
 
 	while (instructionStream.good()) {
@@ -205,56 +201,3 @@ int Instruction::LocationNextInstruction(int a_loc)
 	}
 	return a_loc + 1;
 }
-/*
-Symbol #     Symbol     Location
-0                 a               207
-1                 b               206
-2                 hay             102
-3                 hi              100
-4                 x               106
-5                 y               107
-________________________________________________________
-
-*/
-////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////
-/*
-This is roughly how this program should be interperated:
-________________________________________________________
-
-Press Enter to continue...
-
-Translation of Program:
-
-Location    Contents    Original Statement
-0                                det     100
-100          000005     x        declare  5
-101                     y        declare  88
-206          000555     b        declare  555
-207          000100     a        declare  100
-												;this is a test
-
-100          070100     hi       look     x     ;comment immediately after statement
-101          050100              pack     x
-102          060101     hey      acc      y     ;This will accumulate the contents of y which is hey
-103          080100              write    x
-104          120100              bp       hi
-105          130000              halt
-												; test
-
-								 end
-________________________________________________________
-Press Enter to continue...
-
-Results from emulating program:
-
-? 10
-10
-? 144
-144
-?-7
--7
-
-End of emulation
-*/
